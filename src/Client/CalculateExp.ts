b8793866f982d75c3cd4cd01ab2af1f8ceebe563
ironthir <kubadzimira2002@gmail.com>
2022-06-15 00:06:13 +0200

    Changed structure of files

--- src/Client/CalculateExp.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js";
-import { client } from "../..";
-
-const CalculateExp = (receivedMessage: Message) => {
-  if (client.talkedRecentlyExp.has(receivedMessage.author.id)) return;
-  if (receivedMessage.content.includes("http")) return;
-};
-export default CalculateExp;
