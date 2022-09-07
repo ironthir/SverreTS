1894748d4d8ea40599457145c0c6fa8aa7e67a6e
ironthir <kubadzimira2002@gmail.com>
2022-09-07 21:19:38 +0200

    Added prisma for database querying

--- SverreWeb/sverreweb/pages/Leaderboards/[leaderboard].tsx
+++ SverreWeb/sverreweb/pages/Leaderboards/[leaderboard].tsx
@@ -1,10 +1,9 @@
+import { experiences, PrismaClient } from "@prisma/client";
 import { useMemo } from "react";
 import { Card, Col, Container, Image, Row, Table } from "react-bootstrap";
 import { Serverinfo, UserInfo } from "../../CommonTypes/CommonTypes";
 
 const axios = require("axios");
-//Ah, this, it allows me to make an api call with self signed certificate
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export type ExperienceRow = {
   id: number;
@@ -17,30 +16,31 @@ export type ExperienceRow = {
 };
 
 export async function getStaticIDs() {
-  const ids = await axios.get("https://localhost:7007/GetServerIDs");
-  return ids.data.map((x: any) => {
+  const prisma = new PrismaClient();
+  const ids = await prisma.experiences.findMany();
+  return ids.map((x: any) => {
     return { paths: { leaderboard: x } };
   });
 }
 
 export async function getStaticPaths() {
-  const ids = await axios.get("https://localhost:7007/GetServerIDs");
-  const paths = ids.data.map((x: string) => ({
-    params: { leaderboard: x },
+  const prisma = new PrismaClient();
+  const ids = await prisma.experiences.findMany();
+  const paths = ids.map((x: experiences) => ({
+    params: { leaderboard: x.serverid },
   }));
   return { paths, fallback: false };
 }
 
 export async function getServerData(id: any) {
-  const res = await axios.get("https://localhost:7007/GetServerLeaderboard", {
-    params: { serverID: id },
-  });
-  return { id, ...res };
+  const prisma = new PrismaClient();
+  const lead = await prisma.experiences.findMany({ where: { serverid: id } });
+  return lead;
 }
 
 export async function getStaticProps({ params }: any) {
   const data = await getServerData(params.leaderboard);
-  let arrayOfUserIDs: string[] = data.data.map((x: any) => {
+  let arrayOfUserIDs: string[] = data.map((x: any) => {
     return x.userid;
   });
 
@@ -48,8 +48,7 @@ export async function getStaticProps({ params }: any) {
     `https://discord.com/api/guilds/${params.leaderboard}`,
     {
       headers: {
-        Authorization:
-          "Bot NzE0MTcyMDEwODc5NTE2NzMz.XsqyhA.KMTAtr8k1j--m60gND45tU09v0A",
+        Authorization: `Bot ${process.env.BOT_TOKEN}`,
       },
     }
   );
@@ -58,8 +57,7 @@ export async function getStaticProps({ params }: any) {
     {
       headers: {
         limit: 100,
-        Authorization:
-          "Bot NzE0MTcyMDEwODc5NTE2NzMz.XsqyhA.KMTAtr8k1j--m60gND45tU09v0A",
+        Authorization: `Bot ${process.env.BOT_TOKEN}`,
       },
       params: { limit: "100" },
     }
@@ -67,7 +65,7 @@ export async function getStaticProps({ params }: any) {
 
   return {
     props: {
-      leaderboard: data.data,
+      leaderboard: data,
       serverInfo: serverInfo.data,
       usersInfo: usersInfo.data.filter((x: UserInfo) => {
         return !x.user.bot && arrayOfUserIDs.includes(x.user.id);
